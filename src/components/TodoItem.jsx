44857108a0fd80979f2a4de28c2d73ae5178ff8a
Kuroboy <109130450+Kuroboy@users.noreply.github.com>
2024-06-06 03:13:47 +0700

    Update TodoItem.jsx

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -29,25 +29,6 @@ const TodoItem = ({ todo }) => {
           handleDelete(todo.id);
         }}
       >✖</button>
-
-    <div className='flex justify-between'>
-      <div className='hover:bg-green-500 border-solid border-2 border-gray-400 rounded-xl mt-2 text-xl font-semibold  text-center h-full w-3/4'>
-      <p 
-        onClick={() => {
-          handleStatus(todo.id);
-        }}
-        className={`select-none ${todo.done ? 'bg-green-500 line-through rounded-xl ' : ''}`}>
-        {todo.text}
-      </p>
-      </div>
-      <p
-        className='border-solid border-2 border-gray-400 rounded-full mt-2 text-xl font-semibold  text-center
-        hover:bg-red-600 cursor-pointer select-none rounded-full '
-        onClick={() => {
-          handleDelete(todo.id);
-        }}
-      >✖</p>
-
     </div>
   );
 };
